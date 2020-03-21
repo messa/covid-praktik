0a9c3d52f9acde3319b0b91c91778fdc3dabf221
Petr Messner <petr.messner@gmail.com>
2020-03-21 11:20:59 +0100

    add page draft: dashboard

---
+++ pages/dashboard.js
@@ -0,0 +1,22 @@
+import Link from 'next/link'
+
+function Login() {
+    return (
+      <div>
+        <h1>Evidence vybavení ordinací</h1>
+
+        Ordinace: TODO
+
+        <h2>Zadat aktuální data</h2>
+
+        TODO
+
+        <h2>Přehled dříve zadaných dat</h2>
+
+        TODO
+
+      </div>
+    )
+  }
+
+  export default Login
--- pages/login.js
+++ pages/login.js
@@ -1,9 +1,13 @@
+import Link from 'next/link'
+
 function Login() {
     return (
       <div>
         <h1>Přihlášení</h1>
 
         TODO: přihlačovací formulář
+
+        <p><Limk><a href='/dashboard'>Po přihlášení se člověk dostane na tuto stránku</a></Limk></p>
 
       </div>
     )
