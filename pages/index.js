631f7616a0571f4bcb722b36adcd151534b84727
Petr Messner <petr.messner@gmail.com>
2020-03-21 12:05:30 +0100

    add mongoose and a basic model

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,28 @@
 import Link from 'next/link'
 
-function IndexPage() {
+function IndexPage({ foo }) {
   return (
     <div>
       <h1>Evidence vybavení ordinací</h1>
 
       TODO: nějaký vysvětlující text zde
 
+      {foo}
+
       <p><Link href='/registration'><a>Registrace</a></Link></p>
 
       <p><Link href='/login'><a>Přihlášení</a></Link></p>
     </div>
   )
+}
+
+export async function getServerSideProps(context) {
+  const model = require('../lib/model')
+  return {
+    props: { // will be passed to the page component as props
+      foo: await model.foobar()
+    }
+  }
 }
 
 export default IndexPage
