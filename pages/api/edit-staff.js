571ce5c226b03a00c945fe7623fcb699db0542b0
Petr Messner <petr.messner@gmail.com>
2020-03-22 21:24:11 +0100

    add pages/api/edit-staff.js

---
+++ pages/api/edit-staff.js
@@ -0,0 +1,22 @@
+import { withSession } from '../../lib/decorators'
+import { getUserById, insertStaffState } from '../../lib/model'
+
+export default withSession(async function (req, res) {
+  const user = await getUserById(req.session.get('userId'))
+  if (!user) {
+    return res.status(400).json({ error: 'Nesprávné přihlšovací údaje' })
+  }
+  try {
+    await insertStaffState(user.officeId, {
+      doctorTotalCount: req.body.doctors,
+      doctorQuarantinedCount: req.body.quarantinedDoctors,
+      doctorSickCount: req.body.sickDoctors,
+      nurseTotalCount: req.body.nurses,
+      nurseQuarantinedCount: req.body.quarantinedNurses,
+      nurseSickCount: req.body.sickNurses,
+    })
+  } catch (err) {
+    return res.status(400).json({ error: err.toString() })
+  }
+  res.status(200).json({ ok: true })
+})
