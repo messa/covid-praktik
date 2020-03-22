c870c780f155aba7565981c1b84af7ee0132220f
Petr Messner <petr.messner@gmail.com>
2020-03-22 22:18:33 +0100

    show real data in SupplyHistory

--- components/dashboard/SupplyHistory/index.jsx
+++ components/dashboard/SupplyHistory/index.jsx
@@ -1,54 +1,68 @@
 import React from 'react';
 
 export default function SupplyHistory({ supplyUpdates }) {
-    const equipments = ['ffp3', 'ffp2', 'ochr. obleky', 'brýle/štíty'];
-    const historyData = [
-        {
-            date: new Date('3-20-2020'),
-            equipments: [
-                {label: 'ffp3', count: 10, from_state: 0},
-                {label: 'ffp2', count: 12, from_state: 1},
-                {label: 'ochr. obleky', count: 0, from_state: 0},
-                {label: 'brýle/štíty', count: 5, from_state: 0},
-            ],
-        },
-        {
-            date: new Date('3-21-2020'),
-            equipments: [
-                {label: 'ffp3', count: 8, from_state: 0},
-                {label: 'ffp2', count: 10, from_state: 1},
-                {label: 'ochr. obleky', count: 0, from_state: 0},
-                {label: 'brýle/štíty', count: 3, from_state: 0},
-            ],
-        },
-    ];
+    const equipmentNames = [
+        ['ffp3', 'FFP3'],
+        ['ffp2', 'FFP2'],
+        ['shield', 'Brýle/štíty'],
+        ['suit', 'Ochr. obleky'],
+    ]
 
     return (
         <div>
             <h3>Přehled dříve zadaných dat</h3>
 
-            <pre>{JSON.stringify(supplyUpdates, null, 2)}</pre>
-
             <table>
                 <thead>
                 <tr>
-                    <td>Datum</td>
-                    {equipments.map(eq => (
-                        <td key={eq}>{eq}</td>
+                    <th>Datum</th>
+                    {equipmentNames.map(([name, title]) => (
+                        <th key={name}>{title}</th>
                     ))}
                 </tr>
                 </thead>
                 <tbody>
-                {historyData.reverse().map(({date, equipments}) => (
-                    <tr key={date.toString()}>
-                        <td>{date.toLocaleDateString('cs-CZ')}</td>
-                        {equipments.map(({label, count, from_state}) => (
-                            <td key={label}>{label}: {count} ({from_state})</td>
-                        ))}
-                    </tr>
-                ))}
+                    {supplyUpdates.map(up => (
+                        <tr key={up.date}>
+                            <td>{new Date(up.date).toLocaleString('cs-CZ')}</td>
+                            {equipmentNames.map(([name, title]) => (
+                                <td key={name}>
+                                    <span title={title}>
+                                        +{up[name + 'Received']}
+                                        ({up[name + 'ReceivedFromState']})
+                                        {' '}
+                                        -{up[name + 'Consumed']}
+                                    </span>
+                                </td>
+                            ))}
+                        </tr>
+                    ))}
                 </tbody>
             </table>
         </div>
     );
 }
+
+/*
+
+Co chodi za data v supplyChanges:
+
+[
+  {
+    "date": "2020-03-22T21:03:32.953Z",
+    "ffp3Consumed": 0,
+    "ffp3Received": 0,
+    "ffp3ReceivedFromState": 0,
+    "ffp2Consumed": 0,
+    "ffp2Received": 0,
+    "ffp2ReceivedFromState": 0,
+    "shieldConsumed": 0,
+    "shieldReceived": 0,
+    "shieldReceivedFromState": 0,
+    "suitConsumed": 0,
+    "suitReceived": 0,
+    "suitReceivedFromState": 0
+  },
+]
+
+*/
