b4c8c90d555c758d544bb02ade83895b49f45123
Ondřej Tůma <ondr@centrum.cz>
2020-03-23 22:10:57 +0100

    office address

--- components/dashboard/OfficeInfo/index.jsx
+++ components/dashboard/OfficeInfo/index.jsx
@@ -1,24 +1,26 @@
 import React, {useState} from 'react';
 
 import Button from 'Components/forms/Button';
+import Message from 'Components/Message';
 import ModalWindow from 'Components/ModalWindow';
 import OfficeForm from 'Components/dashboard/OfficeForm';
 
 import officeStates from 'Consts/officeStates';
 
 import styles from './styles.scss';
-import Message from '../../Message';
 
 function OfficeInfo({office}) {
     const modalController = useState(false);
     const stateController = useState(office.stateId);
 
     const state = officeStates.find(({id}) => id === stateController[0]);
-    const {name} = office;
+    const {name, street, city, postalCode} = office;
 
     return (
         <div className={styles.wrapper}>
             <h1>{name}</h1>
+
+            {street && <p>{street}, {postalCode} {city}</p>}
 
             <h2>Režim ordinace <Button className={styles.button} small onClick={() => modalController[1](true)}>Změnit režim ordinace</Button></h2>
             <Message>{state ? <strong>{state.label}</strong> : <i>Nevyplněno</i>}</Message>
