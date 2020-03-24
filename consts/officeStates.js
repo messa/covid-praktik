d041b9e98de5ea82657e98b4fd3de7da1acf918d
Petr Messner <petr.messner@gmail.com>
2020-03-24 11:21:41 +0100

    change id of offieStates

--- consts/officeStates.js
+++ consts/officeStates.js
@@ -1,7 +1,7 @@
 export default [
-    {id: 1, label: 'Běžný režim'},
-    {id: 2, label: 'Omezený (jen akutní pacienti)'},
-    {id: 3, label: 'Jen po telefonické domluvě'},
-    {id: 4, label: 'Jen telefonický kontakt'},
-    {id: 5, label: 'Zavřeno'},
+    {id: 'normal', label: 'Běžný režim'},
+    {id: 'only_accute', label: 'Omezený (jen akutní pacienti)'},
+    {id: 'phone_booked', label: 'Jen po telefonické domluvě'},
+    {id: 'via_phone', label: 'Jen telefonický kontakt'},
+    {id: 'closed', label: 'Zavřeno'},
 ]
